1179b38c72c78b18e5081c05ff312ceb118b0201
kiki <378031327@qq.com>
2026-05-26 21:40:11 +0800

    修改 shell/config_init

--- shell/config_init.sh
+++ shell/config_init.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# PHP FPM Configuration
-sed -i 's/;listen.mode = 0660/listen.mode = 0666/' /etc/php/8.4/fpm/pool.d/www.conf
-sed -i 's/listen = \/run\/php\/php8.4-fpm.sock/listen = \/var\/run\/php-fpm.sock/' /etc/php/8.4/fpm/pool.d/www.conf
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"
 
-# Redis Configuration
-mkdir -p /var/log/redis /var/lib/redis
-chown -R redis:redis /var/log/redis /var/lib/redis
-
-# Beanstalkd Configuration
-mkdir -p /var/lib/beanstalkd /var/log/beanstalkd
-
-# Nginx Configuration
-sed -i 's/user www-data/user root/' /etc/nginx/nginx.conf
-
-# Create necessary directories
-mkdir -p /var/www
-mkdir -p /tmp/php_exception.log /tmp/php_notice.log /tmp/php_module.log
-chmod 777 /tmp/php_exception.log /tmp/php_notice.log /tmp/php_module.log
-
-# MySQL Configuration
-chown -R mysql:mysql /var/lib/mysql /var/log/mysql
-
-# PHP Error Log
-touch /var/log/php-fpm.log
-chmod 666 /var/log/php-fpm.log
+sed -i -e "s/^zlib\.output_compression\ = .*/zlib\.output_compression = \On/g" /etc/php/8.4/fpm/php.ini
+sed -i -e "s/^;max_input_vars\ = .*/max_input_vars\ =\ 20000/g" /etc/php/8.4/fpm/php.ini
+sed -i -e "s/^listen\ = .*/listen = \/var\/run\/php-fpm\.sock/g" /etc/php/8.4/fpm/pool.d/www.conf
+sed -i -e "s/^error_log\ = .*/error_log = \/var\/log\/php-fpm\.log/g" /etc/php/8.4/fpm/php-fpm.conf
+sed -i -e "s/^bind\-address/#bind\-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#general_log/general_log/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^query_cache_limit\ .*/query_cache_limit\ =\ 0M/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^query_cache_size\ .*/query_cache_size\ =\ 0M/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#slow_query_log_file\ .*/slow_query_log_file\ =\ \/var\/log\/mysql\/slow\.log/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#skip\-name\-resolve/skip\-name\-resolve/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "/^slow_query_log_file/a\slow_query_log\ =\ on" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#long_query_time\ .*/long_query_time\ =\ 0\.5/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#log_slow_verbosity\ .*/log_slow_verbosity\ =\ query_plan\,explain/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#log-queries-not-using-indexes.*/log-queries-not-using-indexes\ =\ on/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#log_error/log_error/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^skip_log_error/^#skip_log_error/g" /etc/mysql/mariadb.conf.d/50-mysqld_safe.cnf
+sed -i -e "s/^syslog/^#syslog/g" /etc/mysql/mariadb.conf.d/50-mysqld_safe.cnf
+sed -i -e "s/^#BEANSTALKD_EXTRA=.*/BEANSTALKD_EXTRA=\"-z\ 524280\"/g" /etc/default/beanstalkd
