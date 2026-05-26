052529ed86badb0d21d503c57cda74ff73ed7902
kiki <378031327@qq.com>
2026-05-26 22:13:06 +0800

    优化 mysql 日志文件

--- shell/config_init.sh
+++ shell/config_init.sh
@@ -8,15 +8,14 @@ sed -i -e "s/^listen\ = .*/listen = \/var\/run\/php-fpm\.sock/g" /etc/php/8.4/fp
 sed -i -e "s/^error_log\ = .*/error_log = \/var\/log\/php-fpm\.log/g" /etc/php/8.4/fpm/php-fpm.conf
 sed -i -e "s/^bind\-address/#bind\-address/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i -e "s/^#general_log/general_log/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^query_cache_limit\ .*/query_cache_limit\ =\ 0M/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^query_cache_size\ .*/query_cache_size\ =\ 0M/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^#slow_query_log_file\ .*/slow_query_log_file\ =\ \/var\/log\/mysql\/slow\.log/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#log_slow_query_file\ .*/log_slow_query_file\ =\ \/var\/log\/mysql\/slow\.log/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "s/^#log_slow_query_time\ .*/log_slow_query_time\ =\ 1/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i -e "s/^#skip\-name\-resolve/skip\-name\-resolve/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "/^slow_query_log_file/a\slow_query_log\ =\ on" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^#long_query_time\ .*/long_query_time\ =\ 0\.5/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i -e "/^log_slow_query_file/a\log_slow_query\ =\ 1" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i -e "s/^#log_slow_verbosity\ .*/log_slow_verbosity\ =\ query_plan\,explain/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i -e "s/^#log-queries-not-using-indexes.*/log-queries-not-using-indexes\ =\ on/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i -e "s/^#log_error/log_error/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i -e "s/^skip_log_error/^#skip_log_error/g" /etc/mysql/mariadb.conf.d/50-mysqld_safe.cnf
-sed -i -e "s/^syslog/^#syslog/g" /etc/mysql/mariadb.conf.d/50-mysqld_safe.cnf
 sed -i -e "s/^#BEANSTALKD_EXTRA=.*/BEANSTALKD_EXTRA=\"-z\ 524280\"/g" /etc/default/beanstalkd
+
+mkdir  /var/log/mysql
+chown mysql /var/log/mysql
